bfc13b3321b0c9a73304baf0ee6573ed80e1a1f3
jmdevall <carretilla@gmail.com>
2023-11-08 15:06:51 +0100

    mmb example

---
+++ src/test/java/jmdevall/opencodeplan/application/javadiff/mmb/original/Example.java
@@ -0,0 +1,8 @@
+package jmdevall.opencodeplan.application.javadiff.mmb.original;
+
+public class Example {
+
+	public void helloworld(String who) {
+		System.out.println("foo");
+	}
+}
---
+++ src/test/java/jmdevall/opencodeplan/application/javadiff/mmb/revised/Example.java
@@ -0,0 +1,9 @@
+package jmdevall.opencodeplan.application.javadiff.mmb.revised;
+
+public class Example {
+
+	public void helloworld(String who) {
+		int foo=3;
+		foo=foo+5;
+	}
+}
